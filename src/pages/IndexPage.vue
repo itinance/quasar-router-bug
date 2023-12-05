19ba705352147fd8081268cdd741e3f14d8f3016
Hagen Hübel <hhuebel@itinance.com>
2023-12-05 22:44:48 +0100

    initial

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+
+    <div class="q-pa-md example-row-equal-width">
+      <div class="row">
+        <div>INDEX PAGE</div>
+      </div>
+
+
+      <div class="row">
+        <div>
+          In order to navigate to the test page, you need to manually reload the page after clicking the button.
+          Otherwise, only a white page will be rendered.
+          <q-btn align="left" class="btn-fixed-width" color="primary" label="Go to test page" @click="doRedirect"/>
+        </div>
+      </div>
+
+
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import {Todo, Meta} from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import {defineComponent, ref} from 'vue';
+import {dom} from 'quasar';
+import {useRouter} from 'vue-router';
+
+export default defineComponent({
+  name: 'IndexPage',
+  computed: {
+    dom() {
+      return dom
+    }
+  },
+  components: {},
+  setup() {
+
+    const router = useRouter();
+
+    const doRedirect = () => {
+      console.log('doRedirect');
+      router.push({name: 'test'});
+    }
+    return {doRedirect};
+  }
+});
+</script>
---
+++ src/pages/TestPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+
+    <div class="q-pa-md example-row-equal-width">
+      <div>
+        TEST PAGE
+
+        this will only be rendered after manual page-reload
+
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'TestPage',
+  setup () {
+    return { };
+  }
+});
+</script>
